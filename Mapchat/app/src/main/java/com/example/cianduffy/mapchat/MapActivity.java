aae23e157a5783976a9a945dbecb6f051390c72b
David Newell <d.newell1@outlook.com>
2016-11-12 19:21:32 +0000

    Get messages from Firebase and display on map

--- Mapchat/app/src/main/java/com/example/cianduffy/mapchat/MapActivity.java
+++ Mapchat/app/src/main/java/com/example/cianduffy/mapchat/MapActivity.java
@@ -3,11 +3,20 @@ package com.example.cianduffy.mapchat;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by David on 11/11/2016.
@@ -15,19 +24,69 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private DatabaseReference database;
     GoogleMap map;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.map_activity);
+
+        database = FirebaseDatabase.getInstance().getReference();
+        setUpMapIfNeeded();
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (map == null) {
+            ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        this.map = map;
+        // Check if we were successful in obtaining the map.
+        if (map != null) {
+            setUpMap();
+        }
+    }
+
+    private void setUpMap() {
+        final DatabaseReference ref = database.child("Messages").getRef();
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot msgSnapshot: dataSnapshot.getChildren()) {
+                    Message message = msgSnapshot.getValue(Message.class);
+                    //System.out.println(post);
+                    if (message != null) {
+                        // App 2: Todo: Add a map marker here based on the loc downloaded
+                        double lat = message.latitude;
+                        double lon = message.longitude;
+                        map.addMarker(new MarkerOptions()
+                                .position(new LatLng(lat, lon))
+                                .title(message.messageText));
+                    }
+                }
+                ref.removeEventListener(this);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(53.283912, -9.063874));
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
+        map.moveCamera(center);
+        map.animateCamera(zoom);
     }
 }
---
+++ Mapchat/app/src/main/java/com/example/cianduffy/mapchat/Message.java
@@ -0,0 +1,13 @@
+package com.example.cianduffy.mapchat;
+
+/**
+ * Created by David on 12/11/2016.
+ */
+
+public class Message {
+
+    public String messageText;
+    public double longitude;
+    public double latitude;
+
+}
